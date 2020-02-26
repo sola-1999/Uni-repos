a26811028975661753e16b32f3ab47eeaf68de47
Max Jameson <19702735@students.lincoln.ac.uk>
2020-02-26 22:19:38 +0000

    Initial setup

---
+++ Assessment1/Assessment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+
+namespace Assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Array1_256 = System.IO.File.ReadAllLines("Net_1_256.txt");
+            string[] Array2_256 = System.IO.File.ReadAllLines("Net_2_256.txt");
+            string[] Array3_256 = System.IO.File.ReadAllLines("Net_3_256.txt");
+            string[] Array1_2048 = System.IO.File.ReadAllLines("Net_1_2048.txt");
+            string[] Array2_2048 = System.IO.File.ReadAllLines("Net_2_2048.txt");
+            string[] Array3_2048 = System.IO.File.ReadAllLines("Net_3_2048.txt");//Fetches files from the debug folder
+
+
+        }
+
+    }
+}
