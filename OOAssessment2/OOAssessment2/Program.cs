78755ce9b4bd83f4d9408dbbbe6b851653e81b9b
Max Jameson <19702735@students.lincoln.ac.uk>
2020-03-17 13:47:17 +0000

    ignore

---
+++ OOAssessment2/OOAssessment2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOAssessment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
