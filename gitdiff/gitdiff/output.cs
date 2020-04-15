b0ed4b461e8952b6daa69e491a57acb8777dcec1
Max Jameson <19702735@students.lincoln.ac.uk>
2020-04-15 20:12:43 +0100

    Added output class - needs coding

---
+++ gitdiff/gitdiff/output.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitdiff
+{
+    class output
+    {
+        //stores current string 
+        private List<string> file1 { get; set;  }
+        private List<string> file2 { get; set; }
+
+        //stores file name
+        private string file1name { get; set; }
+        private string file2name { get; set; }
+
+
+
+    }
+}
